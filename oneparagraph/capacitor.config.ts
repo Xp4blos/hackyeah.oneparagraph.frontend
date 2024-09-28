0f3f86aa8289b25cb72734e7dd0a203ab5040e92
Xp4blos <93248835+Xp4blos@users.noreply.github.com>
2024-09-28 17:16:43 +0200

    Android Build Setup
    +android build
    +android sync

---
+++ oneparagraph/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'oneparagraph',
+  webDir: 'dist/oneparagraph/browser',
+};
+
+export default config;
